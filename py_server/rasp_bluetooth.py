ec2d40018e5a5e25326d9b186f990e5b71b95a85
Gabe <gabe@growmyharvest.com>
2020-01-25 14:20:00 -0500

    Included code for communicating with bluetooth

--- py_server/rasp_bluetooth.py
+++ py_server/rasp_bluetooth.py
@@ -24,14 +24,35 @@ def sendMessageTo(targetBluetoothMacAddress):
   sock.close()
 
 def lookUpNearbyBluetoothDevices():
-  print "Searching for nearby bluetooth devices ... "
+  # print "Searching for nearby bluetooth devices ... "
+  # nearby_devices = bluetooth.discover_devices()
+  # for bdaddr in nearby_devices:
+  #   print str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]"
+  #Look for all Bluetooth devices
+
+  print "Searching for devices..."
+  print ""
+  #Create an array with all the MAC
+  #addresses of the detected devices
   nearby_devices = bluetooth.discover_devices()
-  for bdaddr in nearby_devices:
-    print str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]"
+  #Run through all the devices found and list their name
+  num = 0
+  print "Select your device by entering its coresponding number..."
+  for i in nearby_devices:
+  	num+=1
+  	print num , ": " , bluetooth.lookup_name( i )
+
+  #Allow the user to select their Arduino
+  #bluetooth module. They must have paired
+  #it before hand.
+  selection = input("> ") - 1
+  print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
+  bd_addr = nearby_devices[selection]
+
 
 
 lookUpNearbyBluetoothDevices()
 
-print "Provide address:"
-theNum = str(input())
-sendMessageTo(theNum)
+# print "Provide address:"
+# theNum = str(input())
+# sendMessageTo(theNum)
