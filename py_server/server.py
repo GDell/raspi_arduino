a907ad7860997e05578fc6581568bb6b61e21d13
Gabe <gabe@growmyharvest.com>
2020-01-26 02:22:49 -0500

    Use python to find the local ipg

--- py_server/server.py
+++ py_server/server.py
@@ -7,12 +7,24 @@ def hello_world():
     return 'Hello, World!'
 
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 # Function to display hostname and
 # IP address
 def get_Host_name_IP():
     try:
         host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
+        host_ip = get_ip()
         print("Hostname :  ",host_name)
         print("IP : ",host_ip)
     except:
@@ -20,6 +32,9 @@ def get_Host_name_IP():
 
 # Driver code
 get_Host_name_IP() #Function call
+
+
+
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
