5205b64031b3b3487654eb5135770864fee8fc86
Gabe <gabe@growmyharvest.com>
2020-01-26 02:18:43 -0500

    Use python to find the local ipg

--- py_server/server.py
+++ py_server/server.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import socket
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+# Function to display hostname and
+# IP address
+def get_Host_name_IP():
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        print("Hostname :  ",host_name)
+        print("IP : ",host_ip)
+    except:
+        print("Unable to get Hostname and IP")
+
+# Driver code
+get_Host_name_IP() #Function call
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
