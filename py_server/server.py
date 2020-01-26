f80db957cc6569e854c7e2abe33a3e1a099d886c
GDell <gabriel.dellaccio@gmail.com>
2020-01-26 07:17:03 +0000

    Got a flask server running on local internet

--- py_server/server.py
+++ py_server/server.py
@@ -9,4 +9,4 @@ def hello_world():
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='192.168.1.176', port=80, debug=True)
