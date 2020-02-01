ae1019951fa88d1a5d08a17024edf55095486996
Gabe <gabe@growmyharvest.com>
2020-02-01 15:16:41 -0500

    Added functions for firing different colors from array

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -10,16 +10,46 @@ int blue_light_pin = 9;
 String received_data;
 String last_operation;
 
-void RGB_color(int red_light_value, int green_light_value, int blue_light_value) {
+typedef struct {
+  int red_light_value;
+  int green_light_value;
+  int blue_light_value;
+} color;
+
+color red = {255, 0, 0};
+color green = {0, 255, 0};
+color blue = {0, 0, 255};
+color raspberry = {255, 255, 125};
+color cyan = {0, 255, 255};
+color magenta = {255, 0, 255};
+color yellow = {255, 255, 0};
+
+color colors[7] = {
+  red, // Red 0
+  green, // Green 1
+  blue, // Blue 2
+  raspberry, // Raspberry 3
+  cyan, // Cyan 4
+  magenta, // Magenta 5
+  yellow // Yellow 6
+}
+
+void fire_color(int red_light_value, int green_light_value, int blue_light_value) {
   analogWrite(red_light_pin, red_light_value);
   analogWrite(green_light_pin, green_light_value);
   analogWrite(blue_light_pin, blue_light_value);
+}
+
+void select_color(int index) {
+  color selectedColor = colors[index];
+  fire_color(selectedColor[0], selectedColor[1], selectedColor[2]);
 }
 
 void setup() {
   pinMode(red_light_pin, OUTPUT);
   pinMode(green_light_pin, OUTPUT);
   pinMode(blue_light_pin, OUTPUT);
+
   // MAKE SURE THAT THE TX AND RX PINS ARE CONNECTED TO:
   // NOTE: This is only validated for the Elegoo Mega 2560 Third Party Board.
   //       If you are using an Arduino board, the following TX/RX config may
@@ -38,7 +68,15 @@ void loop() { // run over and over
   bool reading = false;
   char recv_data;
 
-  RGB_color(255, 0, 0);
+  select_color(0);
+
+  // fire_color(255, 0, 0); // Red
+  // fire_color(0, 255, 0); // Green
+  // fire_color(0, 0, 255); // Blue
+  // fire_color(255, 255, 125); // Raspberry
+  // fire_color(0, 255, 255); // Cyan
+  // fire_color(255, 0, 255); // Magenta
+  // fire_color(255, 255, 0); // Yellow
 
   if (Serial1.available()) {
     reading = true;
